c3c9c943d2cff0b5fbe2107093de316e19dbf3a4
manisH <manish-resiliencesoft@gmail.com>
2023-06-28 11:12:33 +0530

    Implemented UI fix for Catgeory screen

--- src/screens/Category3Screen.js
+++ src/screens/Category3Screen.js
@@ -1,53 +1,81 @@
-import React, { PureComponent } from 'react'
-import { View, Text, Platform, StyleSheet, TouchableOpacity, Image, ScrollView } from 'react-native'
-import { Icon } from 'native-base'
-import { UIActivityIndicator } from 'react-native-indicators'
-import { CardStyleInterpolators } from 'react-navigation-stack'
-import CategoryFlatList from '../common/CategoriesFlatList'
-import { createSelector } from 'reselect'
-import { connect } from 'react-redux'
-import Header from '../common/HeaderCustom'
-import { appTextStyle } from '../common/Theme.style'
-import downIcon from '../images/down_dark.png'
-import nextIcon from '../images/next_dark.png'
+import React, {PureComponent} from 'react';
+import {
+  View,
+  Text,
+  Platform,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  ScrollView,
+} from 'react-native';
+import {Icon} from 'native-base';
+import {UIActivityIndicator} from 'react-native-indicators';
+import {CardStyleInterpolators} from 'react-navigation-stack';
+import CategoryFlatList from '../common/CategoriesFlatList';
+import {createSelector} from 'reselect';
+import {connect} from 'react-redux';
+import Header from '../common/HeaderCustom';
+import {appTextStyle} from '../common/Theme.style';
+import downIcon from '../images/down_dark.png';
+import nextIcon from '../images/next_dark.png';
 
 class Category3 extends PureComponent {
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       activityIndicatorTemp: false,
       activeId: 0,
       categories: [
-        { id: 1, title: 'Home Supply', imgLink: require('../images/IntroImages/slide2.jpg'), active: false },
-        { id: 2, title: 'Kitchenware &\n Tableware', imgLink: require('../images/IntroImages/slide3.jpg'), active: false },
-        { id: 3, title: 'Electronic', imgLink: require('../images/IntroImages/slide2.jpg'), active: false },
-        { id: 4, title: 'Grocery', imgLink: require('../images/IntroImages/slide3.jpg'), active: false }
-      ]
-    }
+        {
+          id: 1,
+          title: 'Home Supply',
+          imgLink: require('../images/IntroImages/slide2.jpg'),
+          active: false,
+        },
+        {
+          id: 2,
+          title: 'Kitchenware &\n Tableware',
+          imgLink: require('../images/IntroImages/slide3.jpg'),
+          active: false,
+        },
+        {
+          id: 3,
+          title: 'Electronic',
+          imgLink: require('../images/IntroImages/slide2.jpg'),
+          active: false,
+        },
+        {
+          id: 4,
+          title: 'Grocery',
+          imgLink: require('../images/IntroImages/slide3.jpg'),
+          active: false,
+        },
+      ],
+    };
   }
 
-  componentDidMount () {
+  componentDidMount() {
     this.props.navigation.setParams({
       headerTitle: this.props.language.Category,
       colorProps: this.props.themeStyle.primaryBackgroundColor,
-      iconColor: this.props.themeStyle.textColor
-    })
-    this.setState({ activityIndicatorTemp: false })
+      iconColor: this.props.themeStyle.textColor,
+    });
+    this.setState({activityIndicatorTemp: false});
   }
 
-  setActiveItem (value) {
-    let id = value
+  setActiveItem(value) {
+    let id = value;
     if (value === this.state.activeId) {
-      id = 0
+      id = 0;
     }
 
     this.setState(state => ({
       ...state,
-      activeId: id
-    }))
+      activeId: id,
+    }));
   }
 
-  render () {
+  render() {
     // old code
     // return this.state.activityIndicatorTemp ? (
     //   <View
@@ -97,8 +125,17 @@ class Category3 extends PureComponent {
     // )
     return (
       <View style={styles.flexColumn}>
-        <Header searchIcon={true} menuIcon={true} cartIcon={true} navigation={this.props.navigation} />
+        <Header
+          searchIcon={true}
+          menuIcon={true}
+          cartIcon={true}
+          navigation={this.props.navigation}
+        />
         <View style={styles.container}>
+          <Text style={styles.accountTxt}>Account</Text>
+          <Text style={styles.signTxt}>
+            Login/Sign in for the best experience
+          </Text>
           <TouchableOpacity style={styles.loginBtn}>
             <Text style={styles.loginTxt}>Log in</Text>
           </TouchableOpacity>
@@ -107,109 +144,128 @@ class Category3 extends PureComponent {
               <View key={index}>
                 <TouchableOpacity
                   style={[styles.flexRow, styles.cateItem, styles.borderShadow]}
-                  onPress={() => this.setActiveItem(item.id)}
-                >
+                  onPress={() => this.setActiveItem(item.id)}>
                   <View style={styles.flexRow}>
                     <Image source={item.imgLink} style={styles.cateImg} />
                     <Text style={styles.styleTxt}>{item.title}</Text>
                   </View>
-                  <Image source={this.state.activeId === item.id ? nextIcon : downIcon} />
+                  <Image
+                    source={
+                      this.state.activeId === item.id ? nextIcon : downIcon
+                    }
+                    style={{tintColor: '#fdb515'}}
+                  />
                 </TouchableOpacity>
-                { this.state.activeId === item.id &&
+                {this.state.activeId === item.id && (
                   <View style={styles.subContainer}>
                     {this.state.categories.map((subItem, subIndex) => (
-                      <TouchableOpacity key={subIndex} style={[styles.flexRow, styles.subItem, styles.borderShadow]}>
+                      <TouchableOpacity
+                        key={subIndex}
+                        style={[
+                          styles.flexRow,
+                          styles.subItem,
+                          styles.borderShadow,
+                        ]}>
                         <View style={styles.flexRow}>
-                          <Image source={subItem.imgLink} style={styles.subImg} />
+                          <Image
+                            source={subItem.imgLink}
+                            style={styles.subImg}
+                          />
                           <Text style={styles.styleTxt}>{subItem.title}</Text>
                         </View>
-                        <Image source={downIcon} />
+                        <Image
+                          source={downIcon}
+                          style={{tintColor: '#fdb515'}}
+                        />
                       </TouchableOpacity>
                     ))}
                   </View>
-                }
+                )}
               </View>
             ))}
           </ScrollView>
         </View>
       </View>
-    )
+    );
   }
 }
 /// ///////////////////////////////////////////////
 /// ///////////////////////////////////////////////
-const getTheme = (state) => state.appConfig.themeStyle
-const getLanguage = (state) => state.appConfig.languageJson
-const getCategories = (state) => state.getCategories.sortCategory
+const getTheme = state => state.appConfig.themeStyle;
+const getLanguage = state => state.appConfig.languageJson;
+const getCategories = state => state.getCategories.sortCategory;
 const getThemeFun = createSelector(
   [getTheme],
-  (getTheme) => {
-    return getTheme
-  }
-)
+  getTheme => {
+    return getTheme;
+  },
+);
 const getCategoriesFun = createSelector(
   [getCategories],
-  (getCategories) => {
-    return getCategories
-  }
-)
+  getCategories => {
+    return getCategories;
+  },
+);
 const getLanguageFun = createSelector(
   [getLanguage],
-  (getLanguage) => {
-    return getLanguage
-  }
-)
+  getLanguage => {
+    return getLanguage;
+  },
+);
 const mapStateToProps = state => ({
   themeStyle: getThemeFun(state),
   language: getLanguageFun(state),
-  sortCategory: getCategoriesFun(state)
-})
+  sortCategory: getCategoriesFun(state),
+});
 /// //////////////////////////////////////////
-export default connect(mapStateToProps, null)(Category3)
+export default connect(
+  mapStateToProps,
+  null,
+)(Category3);
 const styles = StyleSheet.create({
   indicatorView: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    alignSelf: 'center'
+    alignSelf: 'center',
   },
   emptyProductView: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     alignSelf: 'center',
-    alignContent: 'center'
+    alignContent: 'center',
   },
   emptyProductInnerView: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
     marginBottom: 40,
-    alignSelf: 'center'
+    alignSelf: 'center',
   },
 
   flexRow: {
     display: 'flex',
     flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
   },
   flexColumn: {
     display: 'flex',
-    flexDirection: 'column'
+    flexDirection: 'column',
   },
   borderShadow: {
     elevation: 5,
-    shadowColor: '#171717'
+    shadowColor: '#171717',
   },
   styleTxt: {
     fontWeight: '700',
     color: 'black',
-    fontSize: 22
+    fontSize: 22,
   },
 
   container: {
-    padding: 20
+    padding: 20,
   },
 
   loginBtn: {
@@ -217,42 +273,49 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#fcc922',
-    height: 60
+    height: 60,
   },
   loginTxt: {
     color: 'white',
     fontSize: 22,
-    fontWeight: '700'
+    fontWeight: '700',
   },
 
-  cateContainer: {
-
-  },
+  cateContainer: {},
   cateItem: {
     marginVertical: 10,
     paddingVertical: 10,
     paddingHorizontal: 20,
     backgroundColor: 'white',
-    height: 100
+    height: 100,
   },
   cateImg: {
     width: 100,
     height: 70,
-    marginRight: 20
+    marginRight: 20,
   },
-  subContainer: {
-
-  },
+  subContainer: {},
   subItem: {
     marginVertical: 10,
     paddingVertical: 10,
     paddingHorizontal: 20,
     backgroundColor: 'lightgray',
-    height: 80
+    height: 80,
   },
   subImg: {
     width: 80,
     height: 60,
-    marginRight: 20
-  }
-})
+    marginRight: 20,
+  },
+  accountTxt: {
+    color: 'black',
+    fontSize: 13,
+    fontWeight: '700',
+  },
+  signTxt: {
+    color: 'black',
+    fontSize: 12,
+    fontWeight: '400',
+    marginBottom: 5,
+  },
+});
